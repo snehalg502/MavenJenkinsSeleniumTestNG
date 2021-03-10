6df234cde549699c4e88b2fee472f4e0270d0d59
Snehal <snehal.d@moentek.com>
2021-03-10 10:01:15 +0530

    Adding 3rd test case

--- src/test/java/com/CFAAutomation/tests/TC_Campign_Tests.java
+++ src/test/java/com/CFAAutomation/tests/TC_Campign_Tests.java
@@ -1,19 +1,29 @@
 package com.CFAAutomation.tests;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class TC_Campign_Tests 
 {
+	WebDriver dr;
 	@Test
 	public void test1()
 	{
 		System.out.println("Test case 1");
+		
+		
 	}
 	
 	@Test
 	public void test2()
 	{
 		System.out.println("Test case 2");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Test case 3");
 	}
 	
 }
