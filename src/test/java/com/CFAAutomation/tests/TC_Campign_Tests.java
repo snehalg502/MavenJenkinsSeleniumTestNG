e4776da827d654b2a0ac145082948dd3e034112e
Snehal <snehal.d@moentek.com>
2021-03-10 09:49:57 +0530

    Commiting Maven Project

---
+++ src/test/java/com/CFAAutomation/tests/TC_Campign_Tests.java
@@ -0,0 +1,19 @@
+package com.CFAAutomation.tests;
+
+import org.testng.annotations.Test;
+
+public class TC_Campign_Tests 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Test case 1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Test case 2");
+	}
+	
+}
